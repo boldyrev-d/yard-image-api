2acdc8f3be3919b2e5b6f806912edbb72e65d094
Denis Boldyrev <boldyrev.ds@gmail.com>
2018-01-26 04:04:19 +0300

    chore: add eslint

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
-app.use('/images', express.static(__dirname + '/uploads/'));
+app.use('/images', express.static(`${__dirname}/uploads/`));
 
-app.listen(port, err => {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
     return;
