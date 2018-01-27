184177e529157d24bb716f8bac48fb00f4b29f24
Denis Boldyrev <boldyrev.ds@gmail.com>
2018-01-27 16:54:48 +0300

    feat: change API url, add Access-Control-Allow-Origin header, add extensions option

--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
 
-app.use('/images', express.static(`${__dirname}/uploads/`));
+const options = {
+  extensions: ['jpg', 'jpeg'],
+  setHeaders(res) {
+    res.set('Access-Control-Allow-Origin', 'https://boldyrev-yard.now.sh');
+  },
+};
+
+app.use('/api/v1', express.static(`${__dirname}/uploads/`, options));
 
 app.listen(port, (err) => {
   if (err) {
